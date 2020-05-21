5e5e3e7c1c1d5ded41d904190a5ffc00d92941e4
mostafaahamidmanon <ahmed.hegazy1982@hotmail.com>
2020-05-21 15:11:51 +0200

    Added Seeding Tools. Closes #25

--- back-end/app/config/bundles.php
+++ back-end/app/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
