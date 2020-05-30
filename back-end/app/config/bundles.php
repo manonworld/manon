3545b5f4ca24086944ec0bc08bc4fb3202cb43ef
mostafaahamidmanon <ahmed.hegazy1982@hotmail.com>
2020-05-31 01:45:23 +0200

    Added Swagger and Open API. Closes #56 Closes #54

--- back-end/app/config/bundles.php
+++ back-end/app/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
